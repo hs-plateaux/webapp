fdd85561b54eef9312e44115d06ba59bfd1cae67
Hideki Shimizu <hideki.shimizu@milplateaux.com>
2021-06-24 13:44:43 +0900

    add bcrypt into Gemfile [chapter 6]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: {maximum: 50}
   has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+
 end
 
