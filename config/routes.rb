7ae134e5a0c05deb7df6d0a1757b827bd1ac20d8
Hideki Shimizu <hideki.shimizu@milplateaux.com>
2021-06-24 16:55:40 +0900

    [add] sessions [chapter7]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get 'application/helo'
   get '/help', to: 'static_pages#help'
   get '/about', to:'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
